ba7dda317d146563dde3d32485afa7a81ff2f3aa
Shivanshu Singh <shivanshusingh2203@gmail.com>
2025-09-26 14:25:36 +0530

    Nvim config

---
+++ lua/lsp.lua
@@ -0,0 +1,40 @@
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "lua_ls", "clangd", "pyright" },
+})
+
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+cmp.setup({
+  snippet = {
+    expand = function(args) luasnip.lsp_expand(args.body) end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+  }),
+})
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+for _, server in ipairs({ "lua_ls", "clangd", "pyright" }) do
+  vim.lsp.config(server, {
+    capabilities = capabilities,
+    on_attach = function(_, bufnr)
+      local opts = { buffer = bufnr, noremap = true, silent = true }
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    end,
+  })
+end
+
+require("lsp_signature").setup()
